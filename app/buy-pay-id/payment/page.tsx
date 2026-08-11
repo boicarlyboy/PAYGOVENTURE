70c3e2dc32870e42f8905c09e64d72879a01cf78
boicarlyboy <Boitexas99@gmail.com>
2026-08-11 12:37:28 +0100

    Update account details in payment page

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -30,7 +30,7 @@ export default function PaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("8163980202")
+    navigator.clipboard.writeText("2086059765")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">8163980202</p>
+                <p className="font-bold">2086059765</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">BANKIT MFB</p>
+              <p className="font-bold">KUDA</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">ADEFEMI OLALEKAN </p>
+              <p className="font-bold">FESTUS-TOBI</p>
             </div>
           </div>
 
