b97ce8f988dc62b10b9fb9463851e1a1a457e09d
boicarlyboy <Boitexas99@gmail.com>
2026-08-11 12:44:23 +0100

    Update Telegram support link to new URL

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -27,7 +27,7 @@ export default function SupportPage() {
 
   const handleTelegramSupport = () => {
     // Open Telegram support link in a new tab
-    const telegramUrl = "https://t.me/payment_agent23"
+    const telegramUrl = "https://t.me/Trusted_clinton"
     window.open(telegramUrl, "_blank")
   }
 
